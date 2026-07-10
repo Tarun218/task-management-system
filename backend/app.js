14f0fc9e069377a7d2561cdd66832c9a7eb673f8
Tarun Singodia <tarunmukeshsingodia@gmail.com>
2026-07-10 21:49:06 +0530

    Refactor code structure for improved readability and maintainability

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin:[
-        "http://localhost:5173",
+        "http://localhost:5173","http://localhost:5174",
         "https://task-management-system-ten-murex.vercel.app"
     ],
     credentials: true
